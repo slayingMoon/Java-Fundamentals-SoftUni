a5e2824673b0fe0cde85552e5225d8282207bf57
slayingMoon <g.kolev15@gmail.com>
2021-09-14 19:57:45 +0200

    Upload work

---
+++ Arrays_MoreExercise/RecursiveFibonacci.java
@@ -0,0 +1,18 @@
+package Arrays_MoreExercise;
+
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.println(fib(n));
+    }
+
+    public static long fib(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
