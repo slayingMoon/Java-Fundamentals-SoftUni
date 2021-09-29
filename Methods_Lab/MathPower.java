677fc5c1252126d65f67fd40dcaacaf027b7ce8e
slayingMoon <g.kolev15@gmail.com>
2021-09-29 21:42:39 +0200

    Upload work

---
+++ Methods_Lab/MathPower.java
@@ -0,0 +1,21 @@
+package Methods_Lab;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class MathPower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number = Double.parseDouble(scanner.nextLine());
+        int power = Integer.parseInt(scanner.nextLine());
+        System.out.println(new DecimalFormat("0.####").format(calculateMathPower(number,power)));
+    }
+
+    private static double calculateMathPower(double number, int power) {
+        double result = 1;
+        for (int i = 0; i < power; i++) {
+            result*=number;
+        }
+        return result;
+    }
+}
